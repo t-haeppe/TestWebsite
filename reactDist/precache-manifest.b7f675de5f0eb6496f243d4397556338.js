c970b8c8f24cae691e20dcf4a2a169c1351974b4
JarodAerts-Microsoft <t-jaaert@microsoft.com>
2019-05-31 15:41:53 -0700

    Added Vue and React Dists.

---
+++ reactDist/precache-manifest.b7f675de5f0eb6496f243d4397556338.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2b7c4cee00e889976eb30f0e041450be",
+    "url": "/index.html"
+  },
+  {
+    "revision": "91b6ede7db8c7e620319",
+    "url": "/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "91b6ede7db8c7e620319",
+    "url": "/static/js/main.31c706e1.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
